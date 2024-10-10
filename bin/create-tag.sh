93c0bab24becb1d6c9c482fa985ce0f9d41a26ec
Steven L. Speek <slspeek@gmail.com>
2024-10-10 15:20:25 +0200

    create-tag.sh updated

--- bin/create-tag.sh
+++ bin/create-tag.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# usage: create-tag.sh [-M|-m|-p]
+# usage: create-tag.sh [-M|-m|-p][-d]
 # defaults to -m
 # -M creates new major version
 # -m creates new minor version
 # -p creates new patch version
+# -d dry run
 #
-OPTION=${1:-"-m"}
-
 LAST_TAG=$(git for-each-ref --sort=creatordate --format '%(refname:lstrip=2)' refs/tags|tail -1)
 
 # if no tags where found assume v0.0.0
@@ -24,23 +23,39 @@ increment() {
 echo ------------------------------------------
 echo Latest tag: $LAST_TAG
 
-case $OPTION in
-  "-m" )
-    MINOR=$(increment $MINOR)
-    PATCH=0
-    ;;
-  "-M" )
-    MAJOR=$(increment $MAJOR)
-    MINOR=0
-    PATCH=0
-    ;;
-  "-p" )
-    PATCH=$(increment $PATCH)
-    ;;
-esac
+while getopts "Mmpd" OPT; 
+do
+  case $OPT in
+    m)
+      MINOR=$(increment $MINOR)
+      PATCH=0
+      ;;
+    M)
+      MAJOR=$(increment $MAJOR)
+      MINOR=0
+      PATCH=0
+      ;;
+    p)
+      PATCH=$(increment $PATCH)
+      ;;
+    d)
+      DRY_RUN=1
+      ;;
+  esac
+done
+if [[ "$OPTIND" = "1" ||  ( "$OPTIND" = "2" && "$DRY_RUN" = "1" ) ]]; then # Means no option was given
+      MINOR=$(increment $MINOR)
+      PATCH=0
+fi
+
+
 NEW_TAG=v${MAJOR}.${MINOR}.${PATCH}
 NEW_DESC=$(echo $NEW_TAG|sed -e 's/v/version /')
 echo Creating new tag: $NEW_TAG annotation: $NEW_DESC
 echo ------------------------------------------
 
-git tag -m "$NEW_DESC" $NEW_TAG
+if [ "$DRY_RUN" = "1" ]; then
+  echo Not setting tag as option -d was provided
+else
+  git tag -m "$NEW_DESC" $NEW_TAG
+fi
