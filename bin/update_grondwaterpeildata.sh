ef2d358927203acd75c030e199c8b0544feda422
Steven L. Speek <slspeek@gmail.com>
2024-10-23 14:31:24 +0200

    fix last date retrival

--- bin/update_grondwaterpeildata.sh
+++ bin/update_grondwaterpeildata.sh
@@ -6,7 +6,8 @@ source ./waterdata_utilities.sh
 TODAY=$1
 OLD_DATA=$2
 
-LAST_DATE_FROM_OLD_DATA=$(tail -1 $OLD_DATA |cut -d' ' -f1|sed -e 's/-//g')
+LAST_DATE_FROM_OLD_DATA=$(head -2 $OLD_DATA|tail -1|cut -d' ' -f1|sed -e 's/-//g')
+echo $LAST_DATE_FROM_OLD_DATA
 RETRIEVED_DATA=$(mktemp)
 download_waterdata $LAST_DATE_FROM_OLD_DATA $TODAY >  $RETRIEVED_DATA
 merge_data $OLD_DATA $RETRIEVED_DATA 
