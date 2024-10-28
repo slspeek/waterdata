a2ec1b5fa8aa5bbf3e3c3d4e73758876a8effa0b
Steven L. Speek <slspeek@gmail.com>
2024-10-28 14:37:58 +0100

    1 day overlap in update_grondwaterpeildata.sh

--- bin/update_grondwaterpeildata.sh
+++ bin/update_grondwaterpeildata.sh
@@ -6,9 +6,15 @@ source ./waterdata_utilities.sh
 TODAY=$1
 OLD_DATA=$2
 
-LAST_DATE_FROM_OLD_DATA=$(head -2 $OLD_DATA|tail -1|cut -d' ' -f1|sed -e 's/-//g')
-echo $LAST_DATE_FROM_OLD_DATA
+if [ -z "$3" ]; then
+  LAST_DATE_FROM_OLD_DATA=$(head -2 $OLD_DATA|tail -1|cut -d' ' -f1|sed -e 's/-//g')
+  FROM_DATE=$(date --date "$LAST_DATE_FROM_OLD_DATA -1 days" +%Y%m%d)
+else
+  FROM_DATE=$3
+fi
+
 RETRIEVED_DATA=$(mktemp)
-download_waterdata $LAST_DATE_FROM_OLD_DATA $TODAY >  $RETRIEVED_DATA
+echo Downloading waterdata from $FROM_DATE to $TODAY
+download_waterdata $FROM_DATE $TODAY >  $RETRIEVED_DATA
 merge_data $OLD_DATA $RETRIEVED_DATA 
 
